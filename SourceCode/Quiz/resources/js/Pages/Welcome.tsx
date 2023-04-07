e5fcef764fc927df9507dc78c30f68eff4eaf213
maradamark99 <maradamark99@gmail.com>
2023-04-07 12:03:14 +0200

    feat: change to icon as button from toggle-button

--- SourceCode/Quiz/resources/js/Pages/Welcome.tsx
+++ SourceCode/Quiz/resources/js/Pages/Welcome.tsx
@@ -1,7 +1,5 @@
 import { Link, Head } from "@inertiajs/react";
 import { PageProps } from "@/types";
-import { useState } from "react";
-import { ToggleButton } from "@/Components/ToggleButton";
 import { useDarkMode } from "@/hooks/useDarkMode";
 
 export default function Welcome({
@@ -14,46 +12,42 @@ export default function Welcome({
         <>
             <Head title="Welcome" />
             <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-                <div className="fixed top-6 left-6">
-                    <div className="toggle-container flex justify-center items-center hover:cursor-pointer">
-                        <ToggleButton
-                            label=""
-                            isToggled={isToggled}
-                            handleToggle={() => setIsToggled(!isToggled)}
-                        />
-                        <div className="ml-2">
-                            {isToggled ? (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth={1.5}
-                                    stroke="currentColor"
-                                    className="w-6 h-6 text-cyan-50"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-                                    />
-                                </svg>
-                            ) : (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth={1.5}
-                                    stroke="currentColor"
-                                    className="w-6 h-6 text-gray-600"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-                                    />
-                                </svg>
-                            )}
-                        </div>
+                <div className="fixed top-2 left-6">
+                    <div
+                        className="toggle-container flex justify-center items-center rounded-full hover:cursor-pointer dark:hover:bg-gray-800/50 hover:bg-white/50 p-4"
+                        onClick={() => setIsToggled(!isToggled)}
+                    >
+                        {isToggled ? (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6 text-cyan-50"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                                />
+                            </svg>
+                        ) : (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6 text-gray-600"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                                />
+                            </svg>
+                        )}
                     </div>
                 </div>
                 <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
