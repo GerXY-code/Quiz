213b214e5a0f27774cb640152e0afa73a3fbb436
maradamark99 <maradamark99@gmail.com>
2023-04-09 20:43:50 +0200

    feat: create a mock quiz page

--- SourceCode/Quiz/resources/js/Pages/Quizzes/Quiz.tsx
+++ SourceCode/Quiz/resources/js/Pages/Quizzes/Quiz.tsx
@@ -1,19 +1,40 @@
-import QuizzesLayout from "@/Layouts/QuizzesLayout";
-import { Head } from "@inertiajs/react";
+import PrimaryButton from "@/Components/PrimaryButton";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { User } from "@/types";
+import { useState } from "react";
 
-export default function Quiz({ quizzes }: { quizzes: Quiz[] }) {
-    console.log(quizzes);
+export default function Quiz({ quiz, auth }: { quiz: Quiz; auth: User }) {
+    const [index, setIndex] = useState(0);
+    function increaseIndex() {
+        if (index == quiz.questions.length - 1) return;
+        setIndex(index + 1);
+    }
     return (
-        <QuizzesLayout quizzes={quizzes}>
-            <Head title="Dashboard" />
-
-            <div className="py-12">
+        <AuthenticatedLayout user={auth}>
+            <div className="my-20">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100"></div>
+                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg text-gray-900 dark:text-gray-100">
+                        <div className="p-6">
+                            <h1>{quiz.questions[index].question}</h1>
+                        </div>
+                        <div className="p-6">
+                            <div className="grid grid-cols-2">
+                                {quiz.questions[index].answer.answers.map(
+                                    (ans) => (
+                                        <PrimaryButton
+                                            key={ans}
+                                            className="m-1"
+                                            onClick={() => increaseIndex()}
+                                        >
+                                            {ans}
+                                        </PrimaryButton>
+                                    )
+                                )}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </QuizzesLayout>
+        </AuthenticatedLayout>
     );
 }
