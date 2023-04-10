2dee955ecf5a02c9cabac1910f45497478dacca4
maradamark99 <maradamark99@gmail.com>
2023-04-10 17:02:09 +0200

    fix: change the type of auth to an object that contains an user object

--- SourceCode/Quiz/resources/js/Pages/Quizzes/Quiz.tsx
+++ SourceCode/Quiz/resources/js/Pages/Quizzes/Quiz.tsx
@@ -1,16 +1,16 @@
 import PrimaryButton from "@/Components/PrimaryButton";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { User } from "@/types";
+import { QuizProps } from "@/types/QuizProps";
 import { useState } from "react";
 
-export default function Quiz({ quiz, auth }: { quiz: Quiz; auth: User }) {
+export default function Quiz({ auth, quiz }: QuizProps) {
     const [index, setIndex] = useState(0);
     function increaseIndex() {
         if (index == quiz.questions.length - 1) return;
         setIndex(index + 1);
     }
     return (
-        <AuthenticatedLayout user={auth}>
+        <AuthenticatedLayout user={auth.user}>
             <div className="my-20">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg text-gray-900 dark:text-gray-100">
