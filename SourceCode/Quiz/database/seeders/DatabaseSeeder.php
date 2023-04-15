46959acafbbfc7f73eb2dc2ff4be189119d30fe7
maradamark99 <maradamark99@gmail.com>
2023-04-15 12:11:24 +0200

    feat: make the database seeder call all the seeders in a specified order
    This allows to use the `php artisan db:seed` command alone to run all the seeders, instead of having to do it one by one.

--- SourceCode/Quiz/database/seeders/DatabaseSeeder.php
+++ SourceCode/Quiz/database/seeders/DatabaseSeeder.php
@@ -14,15 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        
-      
-
-
+        $this->call([
+            QuestionSeeder::class,
+            AnswerSeeder::class,
+            QuizSeeder::class,
+            ConnectionSeeder::class
+        ]);
     }
+    
 }
