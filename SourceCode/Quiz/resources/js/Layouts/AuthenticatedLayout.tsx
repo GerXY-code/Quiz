3759d8b38f083558927584b2e141e457dccd93cd
hDani03 <danielhuszka1@gmail.com>
2023-04-18 18:06:01 +0200

    NavBar Főoldal logo helyrerakása

--- SourceCode/Quiz/resources/js/Layouts/AuthenticatedLayout.tsx
+++ SourceCode/Quiz/resources/js/Layouts/AuthenticatedLayout.tsx
@@ -21,7 +21,8 @@ export default function Authenticated({
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
                         <div className="flex">
-                            <div className="shrink-0 flex items-center">
+                            
+                            <div className="shrink-0 flex w-24 items-center justify-end">
                                 <Link href="/">
                                     <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                                 </Link>
