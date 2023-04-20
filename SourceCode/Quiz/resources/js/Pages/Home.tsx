2c3c74c4482d5675fc7ea6fe25cf3554dfc2897b
maradamark99 <maradamark99@gmail.com>
2023-04-20 21:18:52 +0200

    feat: remove fetch and use inertia Link

--- SourceCode/Quiz/resources/js/Pages/Home.tsx
+++ SourceCode/Quiz/resources/js/Pages/Home.tsx
@@ -1,19 +1,8 @@
-import PrimaryButton from "@/Components/PrimaryButton";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { HomeProps } from "@/types/HomeProps";
+import { Link } from "@inertiajs/react";
 
 export default function Home({ auth, quizzes }: HomeProps) {
-    function loadQuiz(id:number){
-        fetch(`/quiz/param?param=${id}`, {
-            method : "GET",
-            /*body: JSON.stringify({
-                getid : id
-            }),*/
-        })
-        .then(response => console.log(response)) 
-    }
-   
-
     return (
         <AuthenticatedLayout user={auth.user}>
             <div className="py-12">
@@ -22,9 +11,16 @@ export default function Home({ auth, quizzes }: HomeProps) {
                         <div className="p-6 text-gray-900 dark:text-gray-100">
                             <div className="grid grid-cols-3 m-2">
                                 {quizzes.map((quiz) => (
-                                    <PrimaryButton className="m-2" onClick={()=>loadQuiz(quiz.id)}>
+                                    <Link
+                                        key={quiz.id}
+                                        href={`/quiz/${quiz.id}`}
+                                        className="m-2 w-56 h-40 flex justify-center items-center px-4 py-2 bg-gray-800 dark:bg-gray-200
+                                        border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest
+                                        hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300
+                                        focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 "
+                                    >
                                         {quiz.title}
-                                    </PrimaryButton>
+                                    </Link>
                                 ))}
                             </div>
                         </div>
