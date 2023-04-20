c3a90e28960dd5885104ef40fe3a77a0ff3bd36d
Gergő Péter Lehoczki <gerixy20@gmail.com>
2023-04-20 22:00:45 +0200

    Update Home.tsx
    Volt bent egy primary button

--- SourceCode/Quiz/resources/js/Pages/Home.tsx
+++ SourceCode/Quiz/resources/js/Pages/Home.tsx
@@ -11,28 +11,25 @@ export default function Home({ auth, quizzes }: HomeProps) {
                         <div className="p-6 text-gray-900 dark:text-gray-100">
                             <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 m-2 flex items-center ">
                                 {quizzes.map((quiz) => (
-                                    <Link
+                                    <Link 
                                         key={quiz.id}
                                         href={`/quiz/${quiz.id}`}
                                         className="m-2 w-56 h-40 flex justify-center items-center px-4 py-2 bg-gray-800 dark:bg-gray-200
                                         border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest
                                         hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300
-                                        focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 "
-                                    >
+                                        focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
+                                    >   
                                         {quiz.title}
                                     </Link>
 
-                                    <PrimaryButton className="m-2 w-64 h-48">
-                                        <div className="mx-auto">
-                                            <p>{quiz.title}</p>
-                                        </div>
-                                    </PrimaryButton>
+
                                 ))}
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            
         </AuthenticatedLayout>
     );
 }
