bf5712a26dca1e298d7f37002fada2d48abf18d5
Juhász Domonkos <j.domonkos01@gmail.com>
2023-04-23 09:06:54 +0200

    Update questions migration

--- SourceCode/Quiz/database/migrations/2023_04_04_143140_questions_migration.php
+++ SourceCode/Quiz/database/migrations/2023_04_04_143140_questions_migration.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('questions', function(Blueprint $table){
                 $table->id();
                 $table->string('question');
+                $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+                $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
