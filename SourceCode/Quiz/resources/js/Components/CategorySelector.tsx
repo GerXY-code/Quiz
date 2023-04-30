f5d5166fff4b2804095b39d556118bbf3340fcc9
maradamark99 <maradamark99@gmail.com>
2023-04-30 20:17:24 +0200

    feat: pass selected category and check its length

--- SourceCode/Quiz/resources/js/Components/CategorySelector.tsx
+++ SourceCode/Quiz/resources/js/Components/CategorySelector.tsx
@@ -3,9 +3,11 @@ import Dropdown from "./Dropdown";
 export default function CategorySelector({
     categories,
     onSelect,
+    selectedCategory,
 }: {
     categories: string[];
     onSelect: (category: string) => void;
+    selectedCategory: string;
 }) {
     return (
         <Dropdown>
@@ -13,9 +15,13 @@ export default function CategorySelector({
                 <span className="inline-flex rounded-md">
                     <button
                         type="button"
-                        className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
+                        className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4
+                        font-medium rounded-md text-gray-200 dark:text-gray-800 bg-gray-800 dark:bg-gray-200
+                        hover:text-gray-300 dark:hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
                     >
-                        Select category
+                        {selectedCategory.length < 1
+                            ? "Select a category"
+                            : selectedCategory}
                         <svg
                             className="ml-2 -mr-0.5 h-4 w-4"
                             xmlns="http://www.w3.org/2000/svg"
@@ -37,7 +43,7 @@ export default function CategorySelector({
                         <button
                             key={category}
                             onClick={() => onSelect(category)}
-                            className="pl-4 pt-1 pb-1 block hover:bg-white"
+                            className="pt-1 pb-1 block w-full hover:bg-gray-800 dark:hover:bg-gray-200 hover:text-gray-200 dark:hover:text-gray-800"
                         >
                             {category}
                         </button>
