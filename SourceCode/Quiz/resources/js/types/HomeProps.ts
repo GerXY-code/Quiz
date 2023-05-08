b99f27fc460aaa855fc93b92016103fb305022be
maradamark99 <maradamark99@gmail.com>
2023-05-08 13:56:08 +0200

    feat: create category type and update homeprops

--- SourceCode/Quiz/resources/js/types/HomeProps.ts
+++ SourceCode/Quiz/resources/js/types/HomeProps.ts
@@ -1,6 +1,8 @@
 import { PageProps } from ".";
 import { Quiz } from "../interfaces/Quiz";
+import { Category } from "./Category";
 
 export type HomeProps = PageProps & {
     quizzes: Quiz[];
+    categories: Category[];
 };
