ec63d2ff81e938c497a4ae3b173fdf31c03dd876
maradamark99 <maradamark99@gmail.com>
2023-05-08 13:57:51 +0200

    feat: add category relationship

--- SourceCode/Quiz/app/Models/Quiz.php
+++ SourceCode/Quiz/app/Models/Quiz.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Quiz extends Model
 {
@@ -15,4 +16,9 @@ class Quiz extends Model
             'quiz_cover',
             'is_private'
     ];
+
+    public function category(): BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
+
 }
