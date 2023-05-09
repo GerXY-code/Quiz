d8ec047304d59eb34a20d245aaf53d77837aea0c
hDani03 <danielhuszka1@gmail.com>
2023-05-09 18:35:26 +0200

    HomePage Stylingolása

--- SourceCode/Quiz/resources/js/Pages/Home.tsx
+++ SourceCode/Quiz/resources/js/Pages/Home.tsx
@@ -19,14 +19,14 @@ export default function Home({ auth, quizzes, categories }: HomeProps) {
         <AuthenticatedLayout user={auth.user}>
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
+                    <div className="bg-sky-600 dark:bg-blue-900 shadow-sm sm:rounded-lg">
                         <div className="p-5 text-gray-900 dark:text-gray-100 flex justify-center">
-                            <div className="ml-12 mt-12">
+                            <div className=" mt-4 mr-6">
                                 <SelectDropdown
                                     values={categories.map((c) => c.category)}
                                     onSelect={handleOnCategorySelect}
                                     selectedValue={""}
-                                    placeholder="Filter by category"
+                                    placeholder="F"
                                 />
                             </div>
                             <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
@@ -34,7 +34,8 @@ export default function Home({ auth, quizzes, categories }: HomeProps) {
                                     <Link
                                         key={quiz.id}
                                         href={`/quiz/${quiz.id}`}
-                                        className="m-5 w-56 h-40 flex justify-center items-center px-4 py-2 bg-gray-800 dark:bg-gray-200
+                                        className="m-5 w-56 h-40 border-solid border-2 flex justify-center items-end px-4 py-10 bg-gradient-to-br from-orange-300 to-amber-300 shadow-2xl 
+                                        dark:bg-gradient-to-br dark:from-amber-700 dark:to-yellow-600
                                         border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest
                                         hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300
                                         focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
